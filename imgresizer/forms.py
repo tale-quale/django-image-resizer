e2bcbd45dc08e4fdc986824d04136d999eb6316c
tale-quale <laptev.ilya@gmail.com>
2020-12-20 13:11:56 +0300

    очистил БД, валидация формы Upload

--- imgresizer/forms.py
+++ imgresizer/forms.py
@@ -12,27 +12,58 @@ def validate_mimetype(url):
     # используя mime type объекта удостоверимся, что это изображение
     url = get_clear_url(url)
     mimetype, _ = mimetypes.guess_type(url)
-    print(mimetype)
+    result = ''
     if mimetype:
-        if not mimetype.startswith('image'):
-            raise ValidationError('Не найдено изображение по ссылке')
+        if mimetype.startswith('image'):
+            result = 'ok' 
+        else:
+            result = 'Не найдено изображение по ссылке'
     else:
-        raise ValidationError('Не найдено изображение по ссылке')
+        result = 'Не найдено изображение по ссылке'
+    
+    return result
 
-def is_url_exist(url):
+def check_url_exist(url):
     try:
         response = requests.get(url) # при несуществующем домене возникает ошибка
     except:
         raise ValidationError('Ссылка не существует')
     if not response.status_code == 200: # домен существует, но не найден сам ресурс
         raise ValidationError('Ссылка не существует')
-    if url != response.url: # редирект?
+    if url != response.url: # редирект как буд-то, хотя при ручном тестировании response.is_redirect был равен False, поэтому сравниваю url'ы
         raise ValidationError('Похоже, что ссылка не существует!')
 
 class UploadForm(forms.Form):
 
     url = forms.URLField(label='Ссылка', required=False)
     file = forms.ImageField(label='Файл', required=False)
+
+    def clean_url(self):
+        url = self.cleaned_data.get('url')
+        if not url:
+            return url
+
+        try:
+            response = requests.get(url)
+        except:
+            self.add_error('url', 'Ссылка не существует')
+
+        if response:
+            if response.status_code != 200:
+                self.add_error('url', 'Ссылка не найдена')
+            else:
+                url_r = response.url
+                mimetype_status = validate_mimetype(url_r)
+                if url != url_r: # если был редирект (response.is_redirect возвращает в некоторых случаях False)
+                    if mimetype_status == 'ok':
+                        url = url_r
+                    else:
+                        self.add_error('url', mimetype_status)
+                else:
+                    if mimetype_status != 'ok':
+                        self.add_error('url', mimetype_status)
+
+        return url
 
     def clean(self):
         cleaned_data = super().clean()
@@ -42,10 +73,6 @@ class UploadForm(forms.Form):
 
         if url and file:
             raise ValidationError('Только одно поле должно быть заполнено')
-
-        if url:
-            is_url_exist(url)
-            validate_mimetype(url)
 
         if not url and not file:
             raise ValidationError('Укажите url или загрузите файл')
